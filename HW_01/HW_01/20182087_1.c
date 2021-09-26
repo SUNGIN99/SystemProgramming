9eba153a2662e4d6abdc8648807f386872559532
성인 김 <99_insung@naver.com>
2021-09-26 09:37:47 +0900

    commit

--- HW_01/HW_01/20182087_1.c
+++ HW_01/HW_01/20182087_1.c
@@ -2,37 +2,27 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#define BUFF_SIZE 500
+#define BUFF_SIZE 1000
 
 int arg_n(char *argv) {
 	int num;
-	if (strcmp("-a", argv) == 0) { // 옵션 -a 라면 모두 출력
-		num = -1;
-		return num;
+	if (strcmp("-a", argv) == 0) {
+		num = -1; //-a 옵션이면 모두 출력
 	}
 
 	else {
-		num = atoi(argv); //문자열을 숫자로 변환
-		//num = (int)(*argv) - '0';
-		if (num <= 26 && num >= 1) {
-			return num;	 //알파벳이 a~z 까지있기 때문에 범위 적용
-		}
-
-		else {
-			fprintf(stderr, "wrong arguments\nuse -a or integer 1~26\n");
-			exit(1);
-		}
+		num = atoi(argv); //문자열을 정수로 변환
 	}
-
+	return num;
 }
-
+//2021.09.26 version 2
 int main(int argc, char *argv[]) {
-	int rfd, n;
-	off_t start;
-	char buf[BUFF_SIZE];
-	rfd = open("abc.txt", O_RDONLY);
-
-
+	int rfd, n, i = 0;
+	int line = 0, line_cnt = 0, read_b = 0;
+	off_t end;
+	char read_all[BUFF_SIZE], buff[BUFF_SIZE];
+	rfd = open("abc", O_RDONLY);
+	//-------------------------------------------------------------------------------
 	if (rfd == -1) { // read file open err
 		fprintf(stderr, "open_err()\n");
 		exit(1);
@@ -43,20 +33,50 @@ int main(int argc, char *argv[]) {
 		exit(1);
 	}
 
-	int line = arg_n(argv[1]); //읽을 줄의 수 (1,2,3 정수)
+	//-------------------------------------------------------------------------------
 
-	if (line == -1) { //읽을 줄의 수가 -1 이라면 모두 출력
+	end = lseek(rfd, 0, SEEK_END);
+	//ex) 0 ~ 155 => why 156?
+	//0~155개의 문자가 있다면 OFFSET은 155를 나타내야하는데 156을 나타낸다 왜일까?
+
+	n = lseek(rfd, 0, SEEK_SET);
+	//파일을 읽기위해 다시 OFFSET을 파일의 처음으로
+
+	n = read(rfd, read_all, end);//0 ~ 155 => char buf[156]
+	//OFFSET 개수에 맞게 읽어야한다. (end가 155였다면 end + 1이 되어야함)
+
+	line = arg_n(argv[1]);
+	if (line == -1) {
 		lseek(rfd, 0, SEEK_SET);
-		line = BUFF_SIZE;
+		read_b = end;
+	}
+	else
+	{
+		for (i = end - 1; i >= 0; i--)
+		{
+			if (read_all[i] == '\n')
+			{	//개행문자가 있을 시 읽어야할 라인 카운트
+				line_cnt++;
+			}
+			if (line_cnt == line + 1)
+			{	//라인카운트가 원하는 조건(출력을 원하는 라인 수)과 맞다면 LOOP_EXIT
+				//if SEEK_END == '\n' => line +1
+				//if seek_END != '\n' = > line
+				//in this problem SEEK_END == '\n'
+				break;
+			}
+			read_b++; //count how many read, 읽을 byte의 수 카운트
+		}
+		n = lseek(rfd, -read_b, SEEK_END);
+		//파일의 끝에서 읽을 카운트 수만큼 앞으로 땡겨줌
+		//SEEK_END가 155였다면,  read_b = read_b - 1 ...?
 	}
 
-	else { //아니면 읽을 줄의수 * 6 (5개의 똑같을 알파벳 + \n = 6개의 문자)
-		start = lseek(rfd, -(line * 6), SEEK_END);
-	}
+	//read_b= OFFSET
+	//printf("offset :%d\n", n);
 
-
-	n = read(rfd, buf, line * 6);
-	write(1, buf, n);
+	n = read(rfd, buff, read_b);
+	write(1, buff, read_b);
 
 	close(rfd);
 	return 0;
--- HW_01/HW_01/20182087_2.c
+++ HW_01/HW_01/20182087_2.c
@@ -1,83 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
 #include <unistd.h>
 #include <fcntl.h>
-#define BUFF_SIZE 1000
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
 
-int arg_n(char *argv) {
-	int num;
-	if (strcmp("-a", argv) == 0) {
-		num = -1; //-a 옵션이면 모두 출력
-	}
 
-	else {
-		num = atoi(argv); //문자열을 정수로 변환
-	}
-	return num;
-}
-//2021.09.26 version 2
 int main(int argc, char *argv[]) {
-	int rfd, n, i = 0;
-	int line = 0, line_cnt = 0, read_b = 0;
-	off_t end;
-	char read_all[BUFF_SIZE], buff[BUFF_SIZE];
-	rfd = open("abc2", O_RDONLY);
-	//-------------------------------------------------------------------------------
-	if (rfd == -1) { // read file open err
-		fprintf(stderr, "open_err()\n");
+	DIR *dp; //디렉토리
+	struct dirent *dirp; //디렉토리 구조체
+	struct stat buf; //파일정보 구조체
+
+	int file = 0, dir = 0, u = 0;
+	char file_path[256]; //파일경로 이름
+
+	if (argc != 2) { 
+		fprintf(stderr, "usage : 20182087_2 directory_name\n");
 		exit(1);
 	}
 
-	if (argc == 1) { //argument count err (have to be upper than 1)
-		fprintf(stderr, "you have to input -a or integers\n");
+	if ((dp = opendir(argv[1])) == NULL) { //존재하지 않는 파일명 전달시 오류
+		fprintf(stderr, "can't open %s\n", argv[1]);
 		exit(1);
 	}
 
-	//-------------------------------------------------------------------------------
-
-	end = lseek(rfd, 0, SEEK_END); 
-	//ex) 0 ~ 155 => why 156?
-	//0~155개의 문자가 있다면 OFFSET은 155를 나타내야하는데 156을 나타낸다 왜일까?
-
-	n = lseek(rfd, 0, SEEK_SET);
-	//파일을 읽기위해 다시 OFFSET을 파일의 처음으로
-
-	n = read(rfd, read_all, end);//0 ~ 155 => char buf[156]
-	//OFFSET 개수에 맞게 읽어야한다. (end가 155였다면 end + 1이 되어야함)
-
-	line = arg_n(argv[1]);
-	if (line == -1) {
-		lseek(rfd, 0, SEEK_SET);
-		read_b = end;
-	}
-	else
-	{
-		for (i = end - 1; i >= 0; i--)
-		{
-			if (read_all[i] == '\n')
-			{	//개행문자가 있을 시 읽어야할 라인 카운트
-				line_cnt++; 
-			}
-			if (line_cnt == line + 1)
-			{	//라인카운트가 원하는 조건(출력을 원하는 라인 수)과 맞다면 LOOP_EXIT
-				//if SEEK_END == '\n' => line +1
-				//if seek_END != '\n' = > line
-				//in this problem SEEK_END == '\n'
-				break;
-			}
-			read_b++; //count how many read, 읽을 byte의 수 카운트
+	while ((dirp = readdir(dp)) != NULL) {
+		strcpy(file_path, argv[1]); //파일경로 복사
+		if (strcmp(dirp->d_name, ".") == 0 || strcmp(dirp->d_name, "..") == 0) {
+			continue; //부모 디렉토리와 현재 디렉토리 제외
 		}
-		n = lseek(rfd, -read_b, SEEK_END); 
-		//파일의 끝에서 읽을 카운트 수만큼 앞으로 땡겨줌
-		//SEEK_END가 155였다면,  read_b = read_b - 1 ...?
+		strcat(file_path, "/"); // 파일경로 밑에 디렉토리 이름 설정
+		strcat(file_path, dirp->d_name);// 파일경로 밑에 있는 디렉토리 이름 추가
+		lstat(file_path, &buf);// 파일의 정보를 buf구조체에 저장
+		//printf("%s\n", file_path);
+		//printf("%s : %d\n %o\n\n", file_path, dirp->d_type, buf.st_mode);
+
+		if (S_ISREG(buf.st_mode)) file++; //파일타입이 regular file 일때 개수 증가
+		else if (S_ISDIR(buf.st_mode)) dir++; //파일타입이 directory일때 개수 증가
+		else u++; //unknown type
 	}
 
-	//read_b= OFFSET
-	//printf("offset :%d\n", n);
-
-	n = read(rfd, buff, read_b);
-	write(1, buff, read_b);
-
-	close(rfd);
+	printf("n dirs : %d\n", dir);
+	printf("n files : %d\n", file);
+	//printf("u : %d\n", u);
 	return 0;
+
 }
