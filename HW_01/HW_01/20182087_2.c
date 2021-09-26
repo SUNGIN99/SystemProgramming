8b893e6b802861b5917d347b7b6156078c4e1da4
성인 김 <99_insung@naver.com>
2021-09-26 09:36:32 +0900

    commit

--- HW_01/HW_01/20182087_2.c
+++ HW_01/HW_01/20182087_2.c
@@ -7,19 +7,19 @@
 int arg_n(char *argv) {
 	int num;
 	if (strcmp("-a", argv) == 0) {
-		num = -1;
+		num = -1; //-a 옵션이면 모두 출력
 	}
 
 	else {
-		num = atoi(argv);
+		num = atoi(argv); //문자열을 정수로 변환
 	}
 	return num;
 }
 //2021.09.26 version 2
 int main(int argc, char *argv[]) {
-	int rfd, n, end = 0, i = 0;
+	int rfd, n, i = 0;
 	int line = 0, line_cnt = 0, read_b = 0;
-	off_t start;
+	off_t end;
 	char read_all[BUFF_SIZE], buff[BUFF_SIZE];
 	rfd = open("abc2", O_RDONLY);
 	//-------------------------------------------------------------------------------
@@ -35,15 +35,15 @@ int main(int argc, char *argv[]) {
 
 	//-------------------------------------------------------------------------------
 
-	end = lseek(rfd, 0, SEEK_END); //0 ~ 155 => why 156?
-	printf("offset end : %d\n", end);
-	int cur;
-	n = lseek(rfd, 0, SEEK_SET);
-	//------------------------------------------WTF
-	n = read(rfd, read_all, end);//0 ~ 155 => char buf[156]
-//------------------------------------------WTF
+	end = lseek(rfd, 0, SEEK_END); 
+	//ex) 0 ~ 155 => why 156?
+	//0~155개의 문자가 있다면 OFFSET은 155를 나타내야하는데 156을 나타낸다 왜일까?
 
-//	printf("read byte : %d\n",n);
+	n = lseek(rfd, 0, SEEK_SET);
+	//파일을 읽기위해 다시 OFFSET을 파일의 처음으로
+
+	n = read(rfd, read_all, end);//0 ~ 155 => char buf[156]
+	//OFFSET 개수에 맞게 읽어야한다. (end가 155였다면 end + 1이 되어야함)
 
 	line = arg_n(argv[1]);
 	if (line == -1) {
@@ -55,28 +55,28 @@ int main(int argc, char *argv[]) {
 		for (i = end - 1; i >= 0; i--)
 		{
 			if (read_all[i] == '\n')
-			{
-				line_cnt++;
+			{	//개행문자가 있을 시 읽어야할 라인 카운트
+				line_cnt++; 
 			}
 			if (line_cnt == line + 1)
-			{
+			{	//라인카운트가 원하는 조건(출력을 원하는 라인 수)과 맞다면 LOOP_EXIT
 				//if SEEK_END == '\n' => line +1
 				//if seek_END != '\n' = > line
 				//in this problem SEEK_END == '\n'
 				break;
 			}
-			read_b++; //count how many read
+			read_b++; //count how many read, 읽을 byte의 수 카운트
 		}
-		n = lseek(rfd, -read_b, SEEK_END);
+		n = lseek(rfd, -read_b, SEEK_END); 
+		//파일의 끝에서 읽을 카운트 수만큼 앞으로 땡겨줌
+		//SEEK_END가 155였다면,  read_b = read_b - 1 ...?
 	}
 
-	//  = OFFSET
-
-	printf("offset :%d\n", n);
+	//read_b= OFFSET
+	//printf("offset :%d\n", n);
 
 	n = read(rfd, buff, read_b);
 	write(1, buff, read_b);
-	printf("line : %d, line_cnt : %d, read_b : %d\n", line, line_cnt, read_b);
 
 	close(rfd);
 	return 0;
